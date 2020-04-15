5af669a7010e1839fd086379185fd88d92c1d9f1
郭悦林 <guoyuelin@q1.com>
2020-04-15 14:13:10 +0800

    未定义相应路由组件时不允许跳转。

--- src/layouts/breadcrumb/index.tsx
+++ src/layouts/breadcrumb/index.tsx
@@ -1,7 +1,10 @@
 import './index.css'
-import React from 'react'
+import React, { useContext, useMemo } from 'react'
 import { Breadcrumb } from 'antd'
 import { useLocation, Link } from 'react-router-dom'
+import { RoutesContext } from '@/context'
+import { flatRoutes, RouteConfig } from '@/routes'
+import { keyBy } from 'lodash'
 
 export interface BreadcrumbProps {
 
@@ -10,11 +13,19 @@ export interface BreadcrumbProps {
 export default function (props: BreadcrumbProps) {
   const location = useLocation();
   const pathSnippets = location.pathname.split('/').filter(i => i);
+  const routes = useContext(RoutesContext);
+  const routeMap = useMemo(() => {
+    return keyBy(flatRoutes(routes), 'absPath');
+  }, [ routes ]);
   const extraBreadcrumbItems = pathSnippets.map((current, index) => {
     const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
+    const route: RouteConfig = routeMap[url];
     return (
       <Breadcrumb.Item key={url}>
-        <Link to={url}>{current}</Link>
+        {
+          route?.component ? <Link to={url}>{current}</Link>
+            : <span style={{cursor: 'not-allowed'}}>{current}</span>
+        }
       </Breadcrumb.Item>
     );
   });
